fb0526309266d5ed5bddf9521fa14455db14f1d8
SamyakJ12 <samyakjayanth@outlook.com>
2024-12-01 18:26:11 -0800

    changed interval to 1wk

--- stock.py
+++ stock.py
@@ -5,7 +5,8 @@ import time
 from datetime import datetime
 
 def fetch_stock_data(ticker, start_date, end_date, file_name=None):
-    stock_data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
+    #valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+    stock_data = yf.download(ticker, start=start_date, end=end_date, interval='1wk')
     
     print("Head of the DataFrame:")
     print(stock_data.head())
